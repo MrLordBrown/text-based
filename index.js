cf6ef6e83dfaa37cb954f1b44257b9720fa42ebc
MrLordBrown <mrlordvondoombraun@gmail.com>
2021-04-01 03:52:30 -0700

    1 file modified

--- index.js
+++ index.js
@@ -22,4 +22,35 @@ function loadData() {
 }
 loadData();
 
+function update() {
+  var count = Object.keys(data).length;
+  console.log(count);
+    var container = document.createElement('table');
+    var head = document.createElement('tr');
+    var post = document.createElement('th');
+    var response = document.createElement('th');
+    var text = document.createElement('th');
+    container.appendChild(head);
+    head.appendChild(post);       
+    head.appendChild(response);
+    head.appendChild(text);
+  for (var i in count) {
+  var = postData
+    var idez = document.createAttribute('id');
+    var classer  = document.createAttribute('class');
+    var sty = document.createAttribute('style');
+    container.setAttributeNode(sty)
+    container.setAttributeNode(idez);
+    container.setAttributeNode(classer);
+    names.textContent = mine[i].Building;
+    weeks.textContent = "Week: "+mine[i].week;
+    idez.value = "week"+i;
+    container.appendChild(names);
+    container.appendChild(weeks);
+    appDiv.appendChild(container);
+    console.log(mine[i].Week);
+    }
+}
+update();
+
 
